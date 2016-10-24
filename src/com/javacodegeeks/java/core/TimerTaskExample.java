9cc9bc7f0661a483228cecd20dd152725f78211b
Administrator <Administrator@172.16.1.131>
2016-10-24 15:30:33 -0400

    initial commit

---
+++ src/com/javacodegeeks/java/core/TimerTaskExample.java
@@ -0,0 +1,90 @@
+package com.javacodegeeks.java.core;
+ 
+
+import java.util.Date;
+import java.util.Timer;
+
+import java.util.TimerTask;
+
+public class TimerTaskExample extends TimerTask {
+ 
+    @Override
+    public void run() {
+
+    	System.out.println(Thread.currentThread());
+    	
+        System.out.println("Start time:" + new Date());
+
+        //
+        doSomeWork();
+
+        System.out.println("End time:" + new Date());
+
+    }
+
+ // simulate a time consuming task
+
+    private void doSomeWork() {
+
+       try {
+
+ 
+
+            Thread.sleep(2000);
+
+ 
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+ 
+
+    public static void main(String args[]) {
+
+ 
+
+        TimerTask timerTask = new TimerTaskExample();
+
+        // running timer task as daemon thread
+
+        Timer timer = new Timer(true);
+
+        timer.scheduleAtFixedRate(timerTask, 0, 3 * 1000);
+
+        System.out.println("TimerTask begins! :" + new Date());
+
+        // cancel after sometime
+
+        try {
+
+            Thread.sleep(20000);
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+
+        //timer.cancel();
+
+        System.out.println("TimerTask cancelled! :" + new Date());
+
+        try {
+
+            Thread.sleep(30000);
+
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+
+        }
+
+    }
+
+ 
+
+}
